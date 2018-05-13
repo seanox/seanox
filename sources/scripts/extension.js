3895d3d875817636e397e4adc188c55ed91e2a13
seanox <service@seanox.de>
2018-05-13 20:52:11 +0200

    #0000

---
+++ sources/scripts/extension.js
@@ -0,0 +1,29 @@
+/**
+ *  Enhancement of the JavaScript API
+ *  Adds a function for creating a alhpanumeric (U)UID with fixed size to the
+ *  Math object.
+ *  @param size optional, default is 16
+ */
+if (Math.uniqueId === undefined) {
+    Math.uniqueId = function(size) {
+        size = size || 16;
+        if (size < 0)
+            sitze = 16;
+        var unique = "";
+        for (var loop = 0; loop < size; loop++) {
+            var random = Math.floor(Math.random() * Math.floor(26));
+            if ((Math.floor(Math.random() * Math.floor(26))) % 2 == 0)
+                unique += String(random % 10);
+            else unique += String.fromCharCode(65 +random); 
+        }
+        return unique;
+    };
+};
+
+/**
+ *  Enhancement of the JavaScript API
+ *  Adds a property to get the UID for the window instance.
+ */  
+if (window.serial === undefined) {
+    window.serial = Math.uniqueId();
+};
